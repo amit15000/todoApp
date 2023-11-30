48ed84525695a184edc8f92aa999cc20668ab2e9
Amit Kumar <15000amitkumar@gmail.com>
2023-11-30 21:52:33 +0530

    Server modified

--- Config/database.js
+++ Config/database.js
@@ -1,3 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(console.log("Server Connection with Database : Successful"))
+  .catch((error) => console.log("Server connection with Database : Failed"));
