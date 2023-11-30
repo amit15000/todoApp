6b8bc86f30f44deec62c57f621b98b28714de8f2
Amit Kumar <15000amitkumar@gmail.com>
2023-11-30 22:53:39 +0530

    Controller created

---
+++ Controller/createTodo.js
@@ -0,0 +1,25 @@
+//import the model(schema)
+const Todo = require("../Models/Todo");
+
+//route-handler
+const createTodo = async (req, res) => {
+  try {
+    //extract title and description from the request
+    const { title, description } = req.body;
+
+    //create a new Todo (schema) object and insert it in database
+    res.status(200).json({
+      success: true,
+      data: response,
+      message: "Entry Created Successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      data: "Internal Server Error",
+      message: err.message,
+    });
+  }
+};
